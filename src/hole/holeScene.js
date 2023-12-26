971f2ffa8bc2fc412c041536e58fbadce0506180
brentbuffham <95211194+brentbuffham@users.noreply.github.com>
2023-12-26 11:26:46 +0800

    Having a play with threes js

--- src/hole/holeScene.js
+++ src/hole/holeScene.js
@@ -1,7 +1,26 @@
-import {drawLines} from "../threeTrail/drawLines.js";
-import {createScene} from "../threeTrail/createScene.js";
+import { drawHole } from "../threeTrail/drawhole.js";
+import { createScene } from "../threeTrail/createScene.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 export function createHole() {
-    const scene = createScene();
-    drawLines(scene);
+	const { scene, camera, renderer } = createScene();
+	drawHole(scene);
+
+	// Add controls
+	const controls = new OrbitControls(camera, renderer.domElement);
+	controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+	controls.dampingFactor = 0.25;
+	controls.screenSpacePanning = false;
+
+	// Animation loop
+	const animate = () => {
+		requestAnimationFrame(animate);
+
+		// Add animation logic here if needed
+		controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+
+		renderer.render(scene, camera);
+	};
+
+	animate();
 }
--- src/threeTrail/createScene.js
+++ src/threeTrail/createScene.js
@@ -1,12 +1,30 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+import { PerspectiveCamera } from "three";
+import { OrthographicCamera } from "three";
 
 export const createScene = () => {
-    const scene = new THREE.Scene();
+	const scene = new THREE.Scene();
 
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+	const renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
 
-    return scene;
-}
+	const frustumSize = 50; // Adjust this value based on your scene size
 
+	const aspect = window.innerWidth / window.innerHeight;
+	const camera = new THREE.OrthographicCamera(
+		frustumSize * aspect / -2, // left
+		frustumSize * aspect / 2, // right
+		frustumSize / 2, // top
+		frustumSize / -2, // bottom
+		0.001, // near
+		1000 // far
+	);
+
+	//const camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.001, 1000);
+
+	camera.position.set(0, 0, 30);
+	camera.lookAt(0, 0, 0);
+
+	return { scene, camera, renderer };
+};
