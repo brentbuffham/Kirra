41facf61cbe14bbc50b6b4af10cd7906e8c6ecb0
brentbuffham <95211194+brentbuffham@users.noreply.github.com>
2023-12-28 21:19:05 +0800

    Some more GUI tools

--- src/drawing/createScene.js
+++ src/drawing/createScene.js
@@ -3,23 +3,26 @@ import { TrackballControls } from "three/examples/jsm/controls/TrackballControls
 import { PerspectiveCamera } from "three";
 import { OrthographicCamera } from "three";
 import Stats from "stats.js";
-import { GUI } from "dat.gui";
+import { GUI } from "lil-gui";
 import { renderFileUpload } from "../file/import/fileUpload";
 
 // ...
 
 let controls, camera, scene, renderer, stats;
+
+const gui = new GUI();
+
 const params = {
 	cameraPerspective: false,
-	upDirection: "Y",
-	rotationAngle: 0 // in radians
+	upDirection: "Z",
+	rotationAngle: 0
 };
 
 export { controls };
 
 export function createScene() {
 	const scene = new THREE.Scene();
-	const renderer = new THREE.WebGLRenderer();
+	const renderer = new THREE.WebGLRenderer({ antialias: false }); // Add the antialias parameter here
 	const canvas = document.querySelector("#canvas");
 	renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
 	document.querySelector("#canvas").appendChild(renderer.domElement);
@@ -33,12 +36,15 @@ export function createScene() {
 
 	// Initialize camera with one of the cameras
 	camera = params.cameraPerspective ? cameraPerspective : cameraOrthographic;
-
 	controls = new TrackballControls(camera, renderer.domElement);
-	controls.rotateSpeed = 20.0;
-	controls.zoomSpeed = 0.5;
-	controls.panSpeed = 1;
-	controls.dynamicDampingFactor = 0.3;
+
+	function setControls() {
+		controls.rotateSpeed = 20.0;
+		controls.zoomSpeed = 0.5;
+		controls.panSpeed = 1;
+		controls.dynamicDampingFactor = 0.3;
+	}
+	setControls();
 
 	function animate() {
 		requestAnimationFrame(animate);
@@ -51,11 +57,25 @@ export function createScene() {
 		frustumSize: 100
 	};
 
-	const gui = new GUI();
-	const perspectiveFolder = gui.addFolder("Perspective Camera");
-	const orthographicFolder = gui.addFolder("Orthographic Camera");
-	const cameraFolder = gui.addFolder("Camera");
-	gui.add(params, "cameraPerspective").name("Perspective View").onChange(function() {
+	// Add a reset Camera button to the GUI
+	gui
+		.add(
+			{
+				resetCamera: function() {
+					// Reset the rotation angle
+					params.rotationAngle = 0;
+					// Reset the camera type
+					params.cameraPerspective = false;
+					// Reset the up direction
+					params.upDirection = "Z";
+					setControls();
+				}
+			},
+			"resetCamera"
+		)
+		.name("Reset Camera");
+
+	gui.add(params, "cameraPerspective").name("Use Perspective Camera").onChange(function() {
 		// Update camera when the perspective checkbox changes
 		camera = params.cameraPerspective ? cameraPerspective : cameraOrthographic;
 		//store the current camera position
@@ -64,14 +84,51 @@ export function createScene() {
 		const target = controls.target;
 		//when the camera is switched, reset the controls
 		controls = new TrackballControls(camera, renderer.domElement);
-		controls.rotateSpeed = 20.0;
-		controls.zoomSpeed = 0.5;
-		controls.panSpeed = 1;
-		controls.dynamicDampingFactor = 0.3;
+		setControls();
 		//set the controls to the stored position and target
 		controls.object.position.copy(position);
 		controls.target.copy(target);
 	});
+	const upOptions = ["X", "Y", "Z"];
+	gui.add(params, "upDirection", upOptions).name("Up Direction Axis").onChange(function() {
+		switch (params.upDirection) {
+			case "Y":
+				camera.up.set(0, 1, 0);
+				break;
+			case "X":
+				camera.up.set(1, 0, 0);
+				break;
+			case "Z":
+				camera.up.set(0, 0, 1);
+				break;
+		}
+		camera.updateProjectionMatrix();
+	});
+	// Store the previous rotation angle
+	let prevRotationAngle = params.rotationAngle;
+
+	// Add a slider for rotation angle in degrees
+	gui.add(params, "rotationAngle", 0, 360).name("View Angle (°)").onChange(function() {
+		// Calculate the delta rotation angle
+		const deltaAngle = params.rotationAngle - prevRotationAngle;
+
+		// Convert delta angle to radians
+		const deltaAngleRad = THREE.MathUtils.degToRad(deltaAngle);
+
+		// Update the previous rotation angle
+		prevRotationAngle = params.rotationAngle;
+
+		// Call the rollCamera function with the delta angle in radians
+		rollCamera(0);
+		rollCamera(deltaAngleRad);
+	});
+
+	const cameraFolder = gui.addFolder("Camera Options");
+	cameraFolder.close();
+
+	const orthographicFolder = cameraFolder.addFolder("Orthographic Camera");
+	const perspectiveFolder = cameraFolder.addFolder("Perspective Camera");
+
 	perspectiveFolder.add(cameraPerspective, "fov", 0, 180).name("Field of View").onChange(function() {
 		cameraPerspective.updateProjectionMatrix();
 	});
@@ -84,6 +141,7 @@ export function createScene() {
 	perspectiveFolder.add(controls, "rotateSpeed", 0.0, 10.0).name("Rotate Speed").onChange(function() {
 		controls.update();
 	});
+
 	orthographicFolder.add(orthographicCameraProps, "frustumSize", 0, 200).name("Frustum Size").onChange(function() {
 		const aspect = canvas.offsetWidth / canvas.offsetHeight;
 		const frustumSize = orthographicCameraProps.frustumSize;
@@ -98,38 +156,6 @@ export function createScene() {
 	});
 	orthographicFolder.add(cameraOrthographic, "far", 0.1, 1000).name("Far Plane").onChange(function() {
 		cameraOrthographic.updateProjectionMatrix();
-	});
-	const upOptions = ["X", "Y", "Z"];
-	cameraFolder.add(params, "upDirection", upOptions).name("Up Direction").onChange(function() {
-		switch (params.upDirection) {
-			case "X":
-				camera.up.set(1, 0, 0);
-				break;
-			case "Y":
-				camera.up.set(0, 1, 0);
-				break;
-			case "Z":
-				camera.up.set(0, 0, 1);
-				break;
-		}
-		camera.updateProjectionMatrix();
-	});
-	// Store the previous rotation angle
-	let prevRotationAngle = params.rotationAngle;
-
-	// Add a slider for rotation angle in degrees
-	cameraFolder.add(params, "rotationAngle", 0, 360).name("Rotation Angle (degrees)").onChange(function() {
-		// Calculate the delta rotation angle
-		const deltaAngle = params.rotationAngle - prevRotationAngle;
-
-		// Convert delta angle to radians
-		const deltaAngleRad = THREE.MathUtils.degToRad(deltaAngle);
-
-		// Update the previous rotation angle
-		prevRotationAngle = params.rotationAngle;
-
-		// Call the rollCamera function with the delta angle in radians
-		rollCamera(deltaAngleRad);
 	});
 
 	function rollCamera(angle) {
